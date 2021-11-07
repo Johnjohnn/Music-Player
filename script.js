e6689bc5e7ec4e1f778c274f7ba7875b16fa82b4
johnjohnn <conde2010@aol.com>
2021-11-06 21:43:26 -0400

    working on javascript

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const image = document.querySelector("img");
+const title = document.querySelector("title");
+const artist= document.querySelector("artist");
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 
-
+// Music 
+const songs = [
+    {
+        name:"jacinto-1",
+        displayName:"Nobember Rain", 
+        artist: "Jacinto Design"
+    }
+]
 
 // Chceck if Playing
 let isPlaying = false;
@@ -30,3 +40,14 @@ function pauseSong() {
 // Play or Pause event listener 
 
 playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()))
+
+// Update Dom 
+function  loadSong (song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src =  `img/${song.name}.jpg`
+}
+
+// On Load - Select First Song 
+loadSong( songs [3]);
